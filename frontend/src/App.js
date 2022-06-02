fbdc898fd57785e6f1d66c3100f59302f7c4d810
pavan-nambi <18B91A12A2@srkrec.ac.in>
2022-06-02 14:18:05 +0530

    calendar layout done

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Modal from "react-modal";
+import Calendar from "./Components/Calendar";
+
+Modal.setAppElement("#root");
+
+function App() {
+  return <Calendar />;
+}
+
+export default App;
